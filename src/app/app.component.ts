ecf96ea98399078b604480487363e1975689a1c6
s.shahin@comptechco.com <s.shahin@comptechco.com>
2023-07-06 00:22:30 +0300

    add new actions to import, export and clear json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,77 @@
-import { Component, ViewChild } from '@angular/core';
-import { JsonEditorComponent, JsonEditorOptions } from "@maaxgr/ang-jsoneditor";
-
+import { AppService } from './shared/services/app.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { JsonEditorComponent, JsonEditorOptions } from '@maaxgr/ang-jsoneditor';
+import {
+  faFileImport,
+  faFileExport,
+  faRemoveFormat,
+} from '@fortawesome/free-solid-svg-icons';
+import exportFromJSON from 'export-from-json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'json-edit-app';
+export class AppComponent implements OnInit {
+  @ViewChild(JsonEditorComponent, { static: false })
+  editor!: JsonEditorComponent;
 
-  @ViewChild(JsonEditorComponent, { static: false }) editor!: JsonEditorComponent;
+  title = 'online json editor';
+  json: any;
 
   public editorOptions: JsonEditorOptions;
   public initialData: any;
   public visibleData: any;
 
-  constructor() {
-    this.editorOptions = new JsonEditorOptions()
-    this.editorOptions.modes = ['code', 'text', 'tree', 'view'];
+  faFileImport = faFileImport;
+  faFileExport = faFileExport;
+  faRemoveFormat = faRemoveFormat;
 
+  constructor(private appService: AppService) {
+    this.editorOptions = new JsonEditorOptions();
+    this.editorOptions.modes = ['code', 'text', 'form', 'tree', 'view'];
     this.initialData = {
-      "success": {
-        "message": "Welcome to JSON Editor",
-        "status_code": 200
-      }
-    }
+      success: {
+        message: 'Welcome to JSON Editor',
+        status_code: 200,
+      },
+    };
+    this.json = this.initialData;
     this.visibleData = this.initialData;
+  }
+
+  ngOnInit(): void {
+    this.appService.jsonData.subscribe((data) => {
+      this.json = data || this.initialData;
+    });
   }
 
   getData(event: any) {
     this.visibleData = event;
+  }
+
+  onFileChange(event: any) {
+    let file = event.target.files[0];
+    let reader = new FileReader();
+    reader.onload = () => {
+      let text: any = reader.result;
+      this.json = JSON.parse(text);
+      this.appService.jsonData.next(this.json);
+    };
+    reader.readAsText(file);
+  }
+
+  saveJson() {
+    let modifiedJson: any = this.editor.get();
+    this.appService.jsonData.next(modifiedJson);
+    let exportData = this.json;
+    let fileName = 'online-json-editor';
+    let exportType: any = 'json';
+    exportFromJSON({ data: exportData, fileName, exportType });
+  }
+
+  clear() {
+    this.appService.jsonData.next(null);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { AngJsoneditorModule } from '@maaxgr/ang-jsoneditor'
+import { AngJsoneditorModule } from '@maaxgr/ang-jsoneditor';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule,
-    AngJsoneditorModule
+    AngJsoneditorModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
